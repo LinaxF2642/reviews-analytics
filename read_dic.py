0898ad9bac87bf9ec708539f78cce4429ea87461
LinaxF2642 <73197340+LinaxF2642@users.noreply.github.com>
2020-12-31 23:07:36 +0000

    Add files via upload

---
+++ read_dic.py
@@ -0,0 +1,59 @@
+
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		 new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
+# 文字計數
+wc = {} # word_count
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # add new key into wc dic
+
+for word in wc:
+	if wc[word] > 100000:
+	print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('沒有這個字喔！')
+
+print('感謝使用本查詢功能')
+
+
